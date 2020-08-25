020b46f16b53815c824d28bf1a91c50ab197dca9
Zoaib Ahmed Khan <thisiszoaib@gmail.com>
2020-08-25 21:23:19 +0500

    Infinite scrolling demo complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, AfterViewInit, NgZone, ChangeDetectorRef } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { map, pairwise, filter, throttleTime } from 'rxjs/operators';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class AppComponent {
-  title = 'angular-infinite-scroll';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('scroller') scroller: CdkVirtualScrollViewport;
+
+  title = 'Angular Infinite Scrolling List';
+
+  listItems = [];
+
+  loading = false;
+
+  constructor(private ngZone: NgZone) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchMore();
+  }
+
+  ngAfterViewInit(): void {
+
+    this.scroller.elementScrolled().pipe(
+      map(() => this.scroller.measureScrollOffset('bottom')),
+      pairwise(),
+      filter(([y1, y2]) => (y2 < y1 && y2 < 140)),
+      throttleTime(200)
+    ).subscribe(() => {
+      this.ngZone.run(() => {
+        this.fetchMore();
+      });
+    }
+    );
+  }
+
+  fetchMore(): void {
+
+    const images = ['IuLgi9PWETU', 'fIq0tET6llw', 'xcBWeU4ybqs', 'YW3F-C5e8SE', 'H90Af2TFqng'];
+
+    const newItems = [];
+    for (let i = 0; i < 20; i++) {
+      const randomListNumber = Math.round(Math.random() * 100);
+      const randomPhotoId = Math.round(Math.random() * 4);
+      newItems.push({
+        title: 'List Item ' + randomListNumber,
+        content: 'This is some description of the list - item # ' + randomListNumber,
+        image: `https://source.unsplash.com/${images[randomPhotoId]}/50x50`
+      });
+    }
+
+    this.loading = true;
+    timer(1000).subscribe(() => {
+      this.loading = false;
+      this.listItems = [...this.listItems, ...newItems];
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
